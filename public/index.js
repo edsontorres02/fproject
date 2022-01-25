6837fcfc570a4b118854a1424cbf46724caa3c1e
edsontorres02 <edson.gonzalez@uttn.mx>
2022-01-25 17:32:59 -0600

    v3

---
+++ public/index.js
@@ -0,0 +1,29 @@
+var students = [
+  {
+    lname: "Gonzalez",
+    name: "Edson Oziel",
+    age: 21,
+    birthday: {
+      year: 2000,
+      month: "February",
+      day: 10
+    },
+    Career: "IT"
+  },
+]
+
+var table = document.getElementsByTagName("table")[0];
+for (var i = 0; i < students.length; i++) {
+  var templ = document.getElementById("tbodytempl").content.cloneNode(true);
+  var tdnumber = templ.querySelector("span[name='tbodynumber']");
+  var tdname = templ.querySelector("span[name='tbodyname']");
+  var tdage = templ.querySelector("span[name='tbodyage']");
+  var tdcareer = templ.querySelector("span[name='tbodycareer']");
+
+  tdnumber.innerText = i + 1;
+  tdname.innerText = students[i].lname + " " + students[i].name;
+  tdage.innerText = students[i].age;
+  tdcareer.innerText = students[i].Career;
+
+  table.append(templ);
+}
